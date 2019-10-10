9e3d46d3eac64b75899bd100f64090f8e4fa8620
Itliusha <1130606318@qq.com>
2019-10-10 01:29:44 +0000

    移动User模型到 app/Models文件夹下

--- public/js/app.js
+++ public/js/app.js
@@ -36973,8 +36973,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/Laravelcode/larabbs/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/Laravelcode/larabbs/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/liusha/Laravelcode/larabbs/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/liusha/Laravelcode/larabbs/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
