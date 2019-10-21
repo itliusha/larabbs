c6431ef8f9cd325d5f44bbda3b7a8c10040eaff7
Itliusha <1130606318@qq.com>
2019-10-21 10:19:08 +0000

    显示头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,22 +25,26 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,'. Auth::id(),
-            'email' => 'required|email',
-            'introduction' => 'max:150',
+            'name'              => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,'. Auth::id(),
+            'email'             => 'required|email',
+            'introduction'      => 'max:150',
+            'avatar'            => 'mimes:png,jpg,jpeg,gif|dimensions:min_width=400,min_height=400',
         ];
     }
 
     public function messages()
     {
         return [
-            'name.required' => '用户名为必填项',
-            'name.regex'    => '用户名只支持英文、数字、‘-’、‘_’',
-            'name.between'  => '用户名必须介于3-25个字符',
-            'name.unique'   => '用户名已存在',
-            'email.required'=> '电子邮箱不能为空',
-            'email.email'   => '电子邮箱格式错误',
-            'introduction'  => '个人简介不能超过80个字符',
+            'name.required'     => '用户名为必填项',
+            'name.regex'        => '用户名只支持英文、数字、‘-’、‘_’',
+            'name.between'      => '用户名必须介于3-25个字符',
+            'name.unique'       => '用户名已存在',
+            'email.required'    => '电子邮箱不能为空',
+            'email.email'       => '电子邮箱格式错误',
+            'introduction'      => '个人简介不能超过80个字符',
+            'avatar.mimes'      => '图片格式必须是png、jpg、jpeg、gif中的一种',
+            'avatar.dimensions' => '图片的宽度需要在400px以上',
+
         ];
     }
 }
