34f8a9f7cc817e961e450dad38e7c96f1f172503
Itliusha <1130606318@qq.com>
2019-10-21 01:20:07 +0000

    用户个人页面

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,9 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+Route::resource('users','UsersController', ['only' => ['show', 'update', 'edit']]);
+// 上面路由等价于下面三条路由
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
