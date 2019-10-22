7cca8e847b58c1e27ace744e19abb0cf3a3b6601
Itliusha <1130606318@qq.com>
2019-10-22 04:45:59 +0000

    话题列表页面渲染

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,16 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    // Category 模型关联
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // User 模型关联
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,9 @@ $factory->define(User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'introduction' => $faker->sentence(),
         'remember_token' => Str::random(10),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
