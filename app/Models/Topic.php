0435e417fdc5f1df53acbbe29ea942e778542d42
Itliusha <1130606318@qq.com>
2019-10-22 12:10:57 +0000

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -17,4 +17,32 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    // 帖子排序
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
+
+            default:
+                $query->recentReplied();
+                break;
+        }
+
+        return $query->with('user', 'category');
+    }
+
+    // 按照更新时间排序
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    // 按照创建时间排序
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
 }
