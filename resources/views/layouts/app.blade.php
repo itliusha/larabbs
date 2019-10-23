14b6bfdc0c285f651d8d1afcd3a97aa0f3365852
Itliusha <1130606318@qq.com>
2019-10-23 06:15:34 +0000

    集成 Simeditor 富文本编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    @yield('styles')
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -22,5 +23,6 @@
     </div>
     <!-- script -->
     <script src="{{ mix('js/app.js') }}"></script>
+    @yield('script')
 </body>
 </html>
